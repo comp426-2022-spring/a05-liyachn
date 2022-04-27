2f09dd34e8cd07bd730839ee96f5dbe3462f462a
liyachn <liyachen@live.unc.edu>
2022-04-26 20:52:45 -0400

    middleware

--- index.js
+++ index.js
@@ -37,23 +37,3 @@ const port = args.port || process.env.PORT || 5000 // if defined => use args, if
 const server = app.listen(port, () => {
     console.log('App running on port %PORT%'.replace('%PORT%', port))
 })
-
-
-
-app.use( (req, res, next) => { // middleware function inserting new record in database
-    let logdata = {
-        remoteaddr: req.ip,
-        remoteuser: req.user,
-        time: Date.now(),
-        method: req.method,
-        url: req.url,
-        protocol: req.protocol,
-        httpversion: req.httpVersion,
-        status: res.statusCode,
-        referer: req.headers['referer'],
-        useragent: req.headers['user-agent']
-    }
-    const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-    next()
-})
