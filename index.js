fa5efd0f9cf3c81c819b008cb81659bbebb70dda
liyachn <liyachen@live.unc.edu>
2022-04-26 20:49:11 -0400

    endpoints

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (args.help || args.h) { // If --help or -h, echo help text to STDOUT and exit
     process.exit(0)
 }
 
-const port = args.port || process.env.PORT || 5555 // if defined => use args, if not defined => use env variable, else use 3000
+const port = args.port || process.env.PORT || 5000 // if defined => use args, if not defined => use env variable, else use 3000
 const server = app.listen(port, () => {
     console.log('App running on port %PORT%'.replace('%PORT%', port))
 })
@@ -102,58 +102,3 @@ function flipACoin(call) {
     }
     return {call: call, flip: flip, result: result}
 }
-
-
-
-app.get('/app/', (req, res) => { // root endpoint
-    res.status(200).end('200 OK') // 200 = success
-    res.type("text/plain")
-})
-
-if (args.debug == true)
-{
-    app.get('/app/log/access/', (req, res) => {
-        const stmt = logdb.prepare('SELECT * FROM accesslog').all() // log all data in accesslog
-        res.status(200).json(stmt)
-        res.type("text/plain")
-    });
-
-    app.get('/app/error/', (req, res) => {
-        throw new Error('Error test successful.')
-    });
-}
-
-if (args.log == true) // create file, write access log to it in combined format
-{
-    // Use morgan for logging to files
-    // Create a write stream to append (flags: 'a') to a file
-    const logstream = fs.createWriteStream('./access.log', { flags: 'a' })
-    // Set up the access logging middleware
-    app.use(morgan('combined', { stream: logstream }))
-}
-
-app.get('/app/flip/', (req, res) => { // returns results of random coin flip
-    res.status(200).json({ 'flip' : coinFlip() }) // add 'flip' key => becomes json
-    res.type("text/plain")
-})
-
-app.get('/app/flips/:number/', (req, res) => { // returns json array of raw random flips + summary
-    const raw = coinFlips(req.params.number)
-    res.status(200).json({ 'raw' : raw, 'summary' : countFlips(raw)})
-    res.type("text/plain")
-})
-
-app.get('/app/flip/call/heads/', (req, res) => { // returns result of random flip match against heads
-    res.status(200).json(flipACoin('heads'))
-    res.type("text/plain")
-})
-
-app.get('/app/flip/call/tails/', (req, res) => { // returns result of random flip match against tails
-    res.status(200).json(flipACoin('tails'))
-    res.type("text/plain")
-})
-
-app.use(function(req, res) { // error if endpoint not found
-    res.status(404).send("404 NOT FOUND")
-    res.type("text/plain")
-})
