5be8f789917e232577dd03c718ddbbf886902699
liyachn <liyachen@live.unc.edu>
2022-04-27 01:04:42 -0400

    guess js

--- public/main.js
+++ public/main.js
@@ -11,15 +11,14 @@ function div(name) { // set active div on click
 
 // Flip one coin and show coin image to match result when button clicked
 function flipCoin() {
-    fetch('http://localhost:5000/app/flip/', {mode: 'cors'})
+    fetch('http://localhost:5000/app/flip/')
     .then(function(response) {
         return response.json();
     })
     .then(function(result) {
         console.log(result);
-        document.getElementById("result").innerHTML = result.flip; // change result text
-        document.getElementById("quarter").setAttribute("src", "./assets/img/"+result.flip+".png"); // change image
-        coin.disabled = true
+        document.getElementById("flipCoinResult").innerHTML = result.flip; // change result text
+        document.getElementById("quarter").setAttribute("src", "assets/img/"+result.flip+".png"); // change image
     })
 }
 
@@ -27,5 +26,29 @@ function flipCoin() {
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
+function flipCoins() {
+    fetch('http://localhost:5000/app/flip/coins', {mode: 'post'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(result) {
+        console.log(result);
+
+        document.getElementById("headsSummary").innerHTML = result.heads; // update summary paragraphs
+        document.getElementById("tailsSummary").innerHTML = result.tails;
+
+        document.getElementById("result").innerHTML = result.flip; // change result text
+        document.getElementById("quarter").setAttribute("src", "./assets/img/"+result.flip+".png"); // change image
+
+        for (var i=0; i < result.raw.length; i++) {
+            var coin = document.createElement("img")
+            coin.setAttribute("src", "assets/img/" + result.raw[i] + ".png");
+            coin.setAttribute("class", "smallcoin")
+        }
+    })
+}
 
 // Guess a flip by clicking either heads or tails button
+function guessFlip(guess) {
+    
+}
